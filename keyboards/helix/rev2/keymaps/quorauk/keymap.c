b958e871321faa17764629a157c538129c403df6
max <m@xprettyjohns.com>
2020-07-23 15:05:22 +0100

    [Keymap] Helix - Add personal keymap

--- keyboards/helix/rev2/keymaps/quorauk/keymap.c
+++ keyboards/helix/rev2/keymaps/quorauk/keymap.c
@@ -39,7 +39,6 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   MOTION,
-  MOTION_TAP,
   BACKLIT,
   EISU,
   KANA,
@@ -70,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
-      KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                    KC_6,   KC_7,   KC_8,    KC_9,    KC_0,   KC_DEL, \
-      KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                    KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,   KC_BSPC, \
-      CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-      KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-      KC_NO,         ADJUST,  KC_LALT, KC_LGUI, KC_LALT, KC_SPC, LOWER,   RAISE,   KC_SPC, MOTION, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      KC_GRV,        KC_1,    KC_2,    KC_3,    KC_4,     KC_5,                    KC_6,   KC_7,    KC_8,    KC_9,    KC_0,   KC_DEL, \
+      KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                    KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC, \
+      CTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
+      KC_LSFT,       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_LBRC, KC_RBRC, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
+      KC_NO,         ADJUST,  KC_LALT, KC_LGUI, KC_LALT, KC_SPC, LOWER,   RAISE,   KC_SPC, MOTION,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Gaming
@@ -271,12 +270,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           layer_on(_MOTION);
         } else {
           layer_off(_MOTION);
-        }
-        return false;
-        break;
-    case MOTION_TAP:
-        if (record->event.pressed) {
-          persistent_default_layer_set(_MOTION);
         }
         return false;
         break;
